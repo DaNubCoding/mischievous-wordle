bf7f741dab80a5a045c481af36d8275330d46a70
Your Name <you@example.com>
2025-07-17 06:28:47 -0400

    initial commit

---
+++ src/lib/components/Tile.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	interface TileProps {
+		letter: string;
+		status: "empty" | "input" | "correct" | "present" | "absent";
+		colIndex: number;
+	}
+
+	let { letter, status, colIndex }: TileProps = $props();
+</script>
+
+<div class="tile {status}">{letter}</div>
+
+<style lang="scss">
+
+@use "$lib/styles/theme.scss" as *;
+
+.tile {
+	display: inline-block;
+	width: 50px;
+	aspect-ratio: 1;
+	text-align: center;
+	font-size: 2rem;
+	border: 2px solid $border-color;
+	margin: 5px;
+
+	&.input {
+		border-color: $input-border-color;
+	}
+
+	&.correct {
+		background-color: $correct-color;
+		border-color: transparent;
+		color: $invert-text-color;
+	}
+
+	&.present {
+		background-color: $present-color;
+		border-color: transparent;
+		color: $invert-text-color;
+	}
+
+	&.absent {
+		background-color: $absent-color;
+		border-color: transparent;
+		color: $invert-text-color;
+	}
+}
+
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import { Wordle } from "$lib";
+</script>
+
+<div id="main-container">
+	<div id="topbar">
+		<h1 id="title">Mischievous Wordle</h1>
+	</div>
+
+	<Wordle />
+</div>
+
+
+
+<style lang="scss">
+
+@use "$lib/styles/theme.scss" as *;
+
+@font-face {
+	font-family: "WordleFont";
+	src: url("neue-helvetica-75-bold.otf") format("opentype");
+}
+
+:root {
+	background-color: $bg-color;
+	color: $text-color;
+	font-family: "WordleFont", sans-serif;
+}
+
+#main-container {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+
+#topbar {
+	display: flex;
+	top: 0;
+	left: 0;
+	width: 100%;
+
+	background-color: var(--bg-color);
+	border-color: #dfdfdf;
+	border-style: solid;
+	border-width: 0 0 1px 0;
+}
+
+#title {
+	text-align: center;
+	flex: 1;
+	font-size: 2.2rem;
+	margin: 1rem;
+}
+
+</style>
